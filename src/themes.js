7a7f62592990f278c6552605b025188df3593be5
Pavel <66474636+Comet-Z@users.noreply.github.com>
2021-11-01 10:31:35 +0300

    Update themes.js

--- src/themes.js
+++ src/themes.js
@@ -1,10 +1,11 @@
-import index from './index'
 
 // * Get theme-switchers from the DOM:
 const defaultTheme = document.querySelector('.default-switch')
 const lightTheme = document.querySelector('.light-switch')
 const darkTheme = document.querySelector('.dark-switch')
 const purpleTheme = document.querySelector('.purple-switch')
+
+const themes = [defaultTheme, lightTheme, darkTheme, purpleTheme]
 
 // * Transform the buttons into an Array: 
 let calcNumbers = Array.from(document.getElementsByTagName('button'))
@@ -14,21 +15,33 @@ let calcNumbers = Array.from(document.getElementsByTagName('button'))
 defaultTheme.addEventListener('click', () => {
     calcNumbers.forEach(element => {
         element.classList.remove('light-theme', 'dark-theme', 'purple-theme')
+        localStorage.clear()
     })
+    
 })
 // ? Light Theme
 lightTheme.addEventListener('click', () => {
     calcNumbers.forEach(element => {
         element.classList.remove('dark-theme', 'purple-theme')
-        element.classList.add('light-theme')
-    })
+        element.classList.add('light-theme') 
+
+        localStorage.setItem('themeLight', 'light-theme')
+        localStorage.getItem('themeLight')
+    })    
+    
 })
+
+
 // ? Dark Theme
 darkTheme.addEventListener('click', () => {
     calcNumbers.forEach(element => {
         element.classList.remove('light-theme', 'purple-theme')
         element.classList.add('dark-theme')
+
+        localStorage.setItem('themeDark', 'dark-theme')
+        localStorage.getItem('themeDark')
     })
+    
 })
 
 // ? Purple Theme
@@ -36,9 +49,29 @@ purpleTheme.addEventListener('click', () => {
     calcNumbers.forEach(element => {
         element.classList.remove('light-theme', 'dark-theme')
         element.classList.add('purple-theme')
+        
     })
 })
 
 
+// ! STORE THEMES IN LOCAL STORAGE :
+// // ? Light Theme :
+// document.onload = function loadLight() {
+//     calcNumbers.forEach(element => {
+//         element.classList.add(localStorage.getItem('themeLight'))
+//     })
+// }
+// // ? Dark Theme :
+// document.onload = function loadDark() {
+//     calcNumbers.forEach(element => {
+//         element.classList.add(localStorage.getItem('themeDark'))
+//     })
+// }
+
+// window.onload = function loadPurple() {
+//     calcNumbers.forEach(element => {
+//         element.classList.add(localStorage.getItem('themePurple'))
+//     })
+// }
 
 
